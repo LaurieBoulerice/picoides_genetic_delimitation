58519049c68c80b20e193ce422a1bde46476b646
Laurie <laurie.boulerice@usherbrooke.ca>
2026-08-25 17:00:37 -0400

    add lgm map script

---
+++ lgm_map.R
@@ -0,0 +1,80 @@
+library(sf)
+library(ggplot2)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(terra)
+
+##########
+#load data
+##########
+
+#ice sheet
+url <- "https://services.arcgis.com/XSeYKQzfXnEgju9o/arcgis/rest/services/Last_Glacial_Maximum/FeatureServer/0/query?where=1%3D1&outFields=*&f=geojson"
+lgm <- st_read(url)
+
+#base map
+world <- ne_countries(
+       scale = "medium",
+       returnclass = "sf"
+   )
+world <- st_transform(world, st_crs(lgm))
+
+###################
+##coastline at LGM
+###################
+
+bathymetryFile <- "https://raw.githubusercontent.com/jorgeassis/rGIS/master/Data/BathymetryDepthMean.tif"
+
+bathymetry <- rast(bathymetryFile)
+
+lgm_raster <- ifel(
+  bathymetry >= -120,
+  1,
+  NA
+)
+
+lgm_landmass <- as.polygons(
+  lgm_raster,
+  dissolve = TRUE,
+  na.rm = TRUE
+)
+
+lgm_landmass <- st_as_sf(lgm_landmass)
+
+lgm_landmass <- st_transform(
+  lgm_landmass,
+  4326
+)
+
+########
+#plot
+#######
+
+ggplot() +
+  geom_sf(
+    data = world,
+    fill = "grey90",
+    color = "grey50",
+    linewidth = 0.3
+  ) +
+  geom_sf(
+    data = lgm_landmass,
+    fill='#E3C98A',
+    color = "#E3C98A",
+    linewidth = 0.3
+  ) +
+  geom_sf(
+    data = lgm,
+    fill = "lightblue",
+    color = "lightblue",
+    alpha = 0.4
+  ) +
+  coord_sf(
+    xlim = c(-180, -40),
+    ylim = c(30, 85),
+    expand = FALSE
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank()
+  )
